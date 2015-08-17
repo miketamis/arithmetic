54c4c32e69c9d72806e950b044fe74687c973541
Joshua Hunter <jhunter@oucs1443.otago.ac.nz>
2015-08-17 22:31:11 +1200

    Created input sanitizer in Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: mtamis
@@ -6,9 +9,38 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Main {
+
     public static void main(String[] args) {
-        BigInteger a = new BigInteger(-5);
-        BigInteger b = new BigInteger(5);
-        System.out.println(a.plus(b));
-    }
+    	Scanner input = new Scanner(System.in);
+		while(input.hasNextLine()) {
+			System.out.println(sanitize(input.nextLine()));
+		} 
+		input.close();
+	}
+
+	public static String sanitize(String s) {
+		String pattern = "(\\d+) ([\\+\\-\\*\\/\\<\\>\\=]|gcd) (\\d+)";
+		Pattern p;
+		Matcher m;
+		
+		try {
+			if (s.charAt(0) == '#') { // If it is a comment
+				return "";
+			}
+		} catch (IndexOutOfBoundsException e) {
+			/*System.err.println("IndexOutOfBoundsException: " +
+				e.getMessage());*/
+			return "Syntax error";
+		}
+
+		p = Pattern.compile(pattern);
+		m = p.matcher(s);
+		if (m.matches()) {
+			return s;
+		} else {
+			return "Syntax error";
+		}
+			
+	}	
+
 }
