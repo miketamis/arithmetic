454ef0bff60592a324a8d1d6f5761b913a22be65
Michael Tamis <mtamis@oucs1446.otago.ac.nz>
2015-08-17 20:30:24 +1200

    Added very light BigInteger

---
+++ src/BigInteger.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mtamis
+ * Date: 8/17/15
+ * Time: 8:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BigInteger {
+
+    ArrayList<Integer> values;
+
+    public BigInteger() {
+        values.add(0);
+    }
+}
