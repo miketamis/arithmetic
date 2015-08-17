80a6bfe017da767d4dda32e1ed0dcf4cbd171751
Michael Tamis <mtamis@oucs1446.otago.ac.nz>
2015-08-17 20:22:17 +1200

    added main file

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mtamis
+ * Date: 8/17/15
+ * Time: 8:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
